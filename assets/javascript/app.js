7d04b595f3f579df1299dcb9efb11d259964ddc9
Max Patten <amanthus2535@gmail.com>
2019-06-06 15:43:53 -0700

    Set up directories, wrote up pseudocode in .txt file and pasted pseudo prompts into html and js files

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//initialize firebase database
+
+//declare variable for database
+
+//create functions for running time calculations
+
+//write block for on click event with submit button
+    //get data from input fields and store in variables
+
+    //push those variables to firebase
+
+    //clear input fields
+
+//write block for .on(child_added) for firebase
+
+    //run calculations for dynamic data variables (next arrival and next train eta)
+
+    //create table row with jQuery
+
+        //append table data pieces to table row with jQuery using firebase data 
+
+    //append table row to table body in html using jQuery
+
