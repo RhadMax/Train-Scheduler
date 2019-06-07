dd59316859da89ec374d660d99aecfe16699bb82
Max Patten <amanthus2535@gmail.com>
2019-06-06 18:06:14 -0700

    Set up html, styled basics, and wrote in JS for submit button functionality with firebase receiving data properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,56 @@
-//initialize firebase database
-
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC7Ou-pxmVa7oKumfddG1PSEJyU4ngTMbU",
+    authDomain: "ucsd-demo-5b2da.firebaseapp.com",
+    databaseURL: "https://ucsd-demo-5b2da.firebaseio.com",
+    projectId: "ucsd-demo-5b2da",
+    storageBucket: "ucsd-demo-5b2da.appspot.com",
+    messagingSenderId: "994117258235",
+    appId: "1:994117258235:web:7c066a0d3507a522"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 //declare variable for database
+var database = firebase.database();
 
 //create functions for running time calculations
 
+
 //write block for on click event with submit button
-    //get data from input fields and store in variables
+$("#add-train").on("click", function (event) {
+    event.preventDefault();
+    console.log("i'm a button and I work!")
+    //get data from input fields and store in variable
+    var trainName = $("#name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var departTime = moment($("#start-input").val().trim(), "HH:mm").format("HH:mm");
+    var trainRate = $("#rate-input").val().trim();
 
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        start: departTime,
+        rate: trainRate
+    };
     //push those variables to firebase
-
+    database.ref().push(newTrain);
     //clear input fields
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#rate-input").val("");
+});
 
 //write block for .on(child_added) for firebase
-
+database.ref().on("child_added", function(snap) {
+    console.log(typeof snap.val().start);
     //run calculations for dynamic data variables (next arrival and next train eta)
-
+    var nextArrival = "exampletime";
+    var minutesAway = "exampletime";
     //create table row with jQuery
 
         //append table data pieces to table row with jQuery using firebase data 
 
     //append table row to table body in html using jQuery
 
+});
