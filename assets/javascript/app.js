efc1f58a1539883dd61fa814f894abd61e9b3863
Max Patten <amanthus2535@gmail.com>
2019-06-06 18:40:12 -0700

    Added in momentJS time conversions using start time and rate from database so all displays are working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,15 +42,24 @@ $("#add-train").on("click", function (event) {
 });
 
 //write block for .on(child_added) for firebase
-database.ref().on("child_added", function(snap) {
+database.ref().on("child_added", function (snap) {
     console.log(typeof snap.val().start);
     //run calculations for dynamic data variables (next arrival and next train eta)
-    var nextArrival = "exampletime";
-    var minutesAway = "exampletime";
-    //create table row with jQuery
+    // var minutesAway = "exampletime";
 
-        //append table data pieces to table row with jQuery using firebase data 
-
+    //LOL that's a lot of momentJS's :P
+    var minutesAway = snap.val().rate - ((moment().diff(moment(moment(snap.val().start, "HH:mm").subtract(1, "years")), "minutes")) % snap.val().rate);
+    var nextArrival = moment(moment().add(minutesAway, "minutes")).format("HH:mm");
+    // var nextArrival = "exampletime";
+    //create table row with jQuery using firebase data 
+    var newRow = $("<tr>").append(
+        //append table data pieces to table row with jQuery
+        $("<td>").text(snap.val().name),
+        $("<td>").text(snap.val().destination),
+        $("<td>").text(snap.val().rate),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minutesAway),
+    );
     //append table row to table body in html using jQuery
-
+    $("tbody").append(newRow);
 });
